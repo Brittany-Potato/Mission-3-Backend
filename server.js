a5d013560236cd7ae754dd85ea30d6317ad5b95a
Brittany-Potato <Brittanycc@missionreadyhq.com>
2025-05-30 15:02:37 +1200

    Got AI integrated and working

--- server.js
+++ server.js
@@ -15,22 +15,43 @@ app.use(express.json());
 // ---------------------------- Varables
 
 const key = process.env.API_KEY;
-const ai = new GoogleGenAI({ key });
+const ai = new GoogleGenAI({ apiKey: key });
 
 // ---------------------------- Simple GET endpoint.
 
-app.get("./hello", (req, res) => {
+app.get("/hello", (req, res) => {
   res.json({ message: "Hello from Node.js!" });
 });
 
 // ---------------------------- Simple POST end point.
 
-app.post("./echo", (req, res) => {
+app.post("/echo", (req, res) => {
   res.json({
     message: "You sent this",
     data: req.body,
   });
 });
+
+
+app.post("/ai", async (req, res) => {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: "Explain how AI works in a few words",
+    });
+    console.log(response.text)
+
+
+main();
+
+  res.json({
+    message: "You sent this",
+    data: req.body,
+  });
+});
+
+
+
+
 
 // ---------------------------- Start the server.
 
@@ -38,19 +59,4 @@ app.listen(port, () => {
   console.log(`Server is listening at http://localhost:3000`);
 });
 
-async function main() {
-  try {
-    const model = ai.getGenerativeModel({ model: "gemini-2.0-flash" });
-    const result = await model.generateContent(
-      "Explain how AI works in a few words"
-    );
-    const response = await result.response;
-    const text = await response.text();
 
-    console.log(response.text);
-  } catch (err) {
-    console.error("Error generating content:", err);
-  }
-}
-
-main();
